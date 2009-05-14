1d45d18705cea28346fab41d77ac6a07f5ca414e
Andrei Bocan <zmaxor@gmail.com>
2009-05-14 14:25:09 +0300

    Try some more

--- lib/tags.rb
+++ lib/tags.rb
@@ -1,12 +1,30 @@
 module Webby
   class Builder
+    # Horrible monkeypatch, forgive me :'(
     alias_method :original_run, :run
 
     def run( opts = {} )
+      puts "The ugly before"
       original_run(opts)
-      puts "Goodbye moto"
-      Resources.pages.each do |file|
-        p file.tags
+      puts "The lovely after"
+      display_tags
+    end
+
+    def display_tags
+      posts = Resources.pages.find(:all,
+                :in_directory => '/',
+                :recursive => true,
+                :blog_post => true)
+
+      tags = posts.map{|p| p.tags}.flatten.uniq.each do |tag|
+        dir = Webby.site.tags_dir
+        p dir
+        page = File.join(dir, File.basename(tag))
+        p page
+        page = Webby::Builder.create(page,
+                :from => "#{Webby.site.template_dir}/blog/tag.erb",
+                :locals => {:tag => tag, :directory => dir})
+
       end
     end
   end
